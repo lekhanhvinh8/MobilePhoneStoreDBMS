cd307ac03847730b704c73d6e826ebd8feaf13e7
Hung Do <beekunfar@gmai.com>
2020-11-23 10:20:53 +0700

    Add new layout(error)

---
+++ MobilePhoneStoreDBMS/MobilePhoneStoreDBMS/Controllers/HomeScreenController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MobilePhoneStoreDBMS.Controllers
+{
+    public class HomeScreenController : Controller
+    {
+        // GET: HomeScreen
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
