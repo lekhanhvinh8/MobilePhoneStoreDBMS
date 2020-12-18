61d73798123f6396d0673446cd155e4fee99dfe4
Hung Do <beekunfar@gmai.com>
2020-12-18 15:30:07 +0700

    Fix ảnh các trang

--- MobilePhoneStoreDBMS/MobilePhoneStoreDBMS/Models/Entities/MobilePhoneStoreDBMSContext.Designer.cs
+++ MobilePhoneStoreDBMS/MobilePhoneStoreDBMS/Models/Entities/MobilePhoneStoreDBMSContext.Designer.cs
@@ -1,4 +1,4 @@
-﻿// T4 code generation is enabled for model 'C:\MyData\OnGoingProject\MobilePhoneStoreDBMS\MobilePhoneStoreDBMS\MobilePhoneStoreDBMS\Models\Entities\MobilePhoneStoreDBMSContext.edmx'. 
+﻿// T4 code generation is enabled for model 'D:\HK1_2020-2021\Database Management System\Đồ án\MobilePhoneStoreDBMS\MobilePhoneStoreDBMS\MobilePhoneStoreDBMS\Models\Entities\MobilePhoneStoreDBMSContext.edmx'. 
 // To enable legacy code generation, change the value of the 'Code Generation Strategy' designer
 // property to 'Legacy ObjectContext'. This property is available in the Properties Window when the model
 // is open in the designer.
