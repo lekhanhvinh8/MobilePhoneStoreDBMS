26e0e7a3aa762e0a3d99a4af2985de852b29383f
Tuan Nhan Nguyen <tuannhan6@gmail.com>
2020-12-18 10:26:16 +0700

    shoppingcart update

--- MobilePhoneStoreDBMS/MobilePhoneStoreDBMS/Controllers/ShoppingCartController.cs
+++ MobilePhoneStoreDBMS/MobilePhoneStoreDBMS/Controllers/ShoppingCartController.cs
@@ -6,6 +6,7 @@ using System.Data.Entity.Migrations;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using MobilePhoneStoreDBMS.Models.Consts;
 
 namespace MobilePhoneStoreDBMS.Controllers
 {
@@ -16,12 +17,13 @@ namespace MobilePhoneStoreDBMS.Controllers
         // add item vao gio hang
         public ActionResult AddtoCart(int id)
         {
-            if (Session["user_id"] == null)
+            if (Session[SessionNames.CustomerID] == null)
             {
-                return RedirectToAction("Index", "Login");
+                return RedirectToAction("Login", "Account");
             }
 
-            int userid = Convert.ToInt32(Session["user_id"]);
+            int userid = Convert.ToInt32(Session[SessionNames.CustomerID]);
+
             Cart c = _db.Carts.Where(x => x.CustomerID == userid && x.ProductID == id).SingleOrDefault();
 
             if (c == null)
@@ -45,14 +47,14 @@ namespace MobilePhoneStoreDBMS.Controllers
         // trang gio hang
         public ActionResult ShowToCart()
         {
-            int userid = Convert.ToInt32(Session["user_id"]);
+            int userid = Convert.ToInt32(Session[SessionNames.CustomerID]);
             List<Cart> cart = _db.Carts.Where(x => x.CustomerID == userid).ToList();
             return View(cart);
         }
         //
         public ActionResult Update_Quantity_Cart(int ID_Product, int quantity)
         {
-            int userid = Convert.ToInt32(Session["user_id"]);
+            int userid = Convert.ToInt32(Session[SessionNames.CustomerID]);
             Cart cart = _db.Carts.Where(x => x.CustomerID == userid && x.ProductID == ID_Product).SingleOrDefault();
             cart.amount = quantity;
             _db.Carts.AddOrUpdate(cart);
@@ -62,7 +64,7 @@ namespace MobilePhoneStoreDBMS.Controllers
         //
         public ActionResult RemoveCart(int id)
         {
-            int userid = Convert.ToInt32(Session["user_id"]);
+            int userid = Convert.ToInt32(Session[SessionNames.CustomerID]);
             Cart cart = _db.Carts.Where(x => x.CustomerID == userid && x.ProductID == id).SingleOrDefault();
             _db.Carts.Remove(cart);
             _db.SaveChanges();
